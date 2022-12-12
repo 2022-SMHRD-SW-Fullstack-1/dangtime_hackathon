12fb192ae7b487d079238d598c33695267062372
JParkBro <hyeongjoopark0324@gmail.com>
2022-12-12 14:27:25 +0900

    chatlist.xml

--- app/src/main/java/com/example/dangtime/chat/ChatListActivity.kt
+++ app/src/main/java/com/example/dangtime/chat/ChatListActivity.kt
@@ -2,11 +2,15 @@ package com.example.dangtime.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.dangtime.R
 
 class ChatListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
+
+
+        val tvChat = findViewById<TextView>(R.id.tvChat)
     }
 }
