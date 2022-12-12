3c3fba0b789442807122ab420fd7d2637f13b2fa
smhrdPjh <pjh01082037858@gmail.com>
2022-12-12 15:15:57 +0900

    test3

--- app/src/main/java/com/example/dangtime/board/BoardWriteMateActivity.kt
+++ app/src/main/java/com/example/dangtime/board/BoardWriteMateActivity.kt
@@ -1,19 +1,15 @@
 package com.example.dangtime.board
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.dangtime.R
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import java.io.ByteArrayOutputStream
 
 class BoardWriteMateActivity : AppCompatActivity() {
 
@@ -25,9 +21,11 @@ class BoardWriteMateActivity : AppCompatActivity() {
 
 
         // id값 다 찾아오기
-        imgLoad = findViewById(R.id.imgLoad)
-        val etContent = findViewById<EditText>(R.id.etWriteMateContent)
+        imgLoad = findViewById(R.id.imgMateLoad)
+        val etContent = findViewById<EditText>(R.id.etWriteStoryContent)
         val tvTo = findViewById<TextView>(R.id.tvWriteMateTitleTo)
+
+        val btnUpload = findViewById<Button>(R.id.btnWriteMateUpload)
 
 
 
@@ -38,62 +36,62 @@ class BoardWriteMateActivity : AppCompatActivity() {
             launcher.launch(intent)
         }
 
-        // 모든 값(title content time...image)을 Firebase에 저장시켜줘야함
-        imgWrite.setOnClickListener{
-            val title = etTitle.text.toString()
-            val content = etContent.text.toString()
-
-            // board
-            // - key(게시물의 고유한 uid : push())
-            //      -boardVO(title, content, 사용자uid, time)
-
-
-            //FBdatabase.getBoardRef().push().setValue(BoardVO("1","1","1","1"))
-
-            // auth
-            val uid = FBAuth.getUid()
-            val time = FBAuth.getTime()
-
-            // setValue가 되기전에 미리 BoardVO가 저장될 key값(uid_)을 만들자
-
-            //먼저 uid를 만들고  key저장
-            var key = FBdatabase.getBoardRef().push().key.toString()
-
-            // boardRef의 uid 밑에 data 저장
-            FBdatabase.getBoardRef().child(key).setValue(BoardVO(title, content, uid, time))
-            imgUpload(key)
-            finish()// 이전페이지로 돌아가기
-        }
-
-    }
-
-    fun imgUpload(key : String){
-        // realtime(boardVO)로 저장되는 값 과 storage에 저장되는 값이 매칭될 수 없음
-        // 그래서 storage의 이미지 이름을 게시물uid.png 식으로  저장해논다
-        //  push()가 uid만듬
-
-        val storeage = Firebase.storage
-        val storageRef = storeage.reference
-        val mountainsRef = storageRef.child("$key.png")
-
-        // Get the data from an ImageView as bytes
-        imgLoad.isDrawingCacheEnabled = true
-        imgLoad.buildDrawingCache()
-        val bitmap = (imgLoad.drawable as BitmapDrawable).bitmap
-        val baos = ByteArrayOutputStream()
-
-        // 이미지 압축   png로 못받아옴 가져오는타입 , 압축의 퀄리티 범위
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos)
-        val data = baos.toByteArray()
-
-        // mountainsRef : 이미지를 어디 경로에 설정할것인지
-        var uploadTask = mountainsRef.putBytes(data)
-        uploadTask.addOnFailureListener {
-            // Handle unsuccessful uploads
-        }.addOnSuccessListener { taskSnapshot ->
-            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
-            // ...
-        }
+//        // 모든 값(title content time...image)을 Firebase에 저장시켜줘야함
+//        btnUpload.setOnClickListener{
+//            val title = etTitle.text.toString()
+//            val content = etContent.text.toString()
+//
+//            // board
+//            // - key(게시물의 고유한 uid : push())
+//            //      -boardVO(title, content, 사용자uid, time)
+//
+//
+//            //FBdatabase.getBoardRef().push().setValue(BoardVO("1","1","1","1"))
+//
+//            // auth
+//            val uid = FBAuth.getUid()
+//            val time = FBAuth.getTime()
+//
+//            // setValue가 되기전에 미리 BoardVO가 저장될 key값(uid_)을 만들자
+//
+//            //먼저 uid를 만들고  key저장
+//            var key = FBdatabase.getBoardRef().push().key.toString()
+//
+//            // boardRef의 uid 밑에 data 저장
+//            FBdatabase.getBoardRef().child(key).setValue(BoardVO(title, content, uid, time))
+//            imgUpload(key)
+//            finish()// 이전페이지로 돌아가기
+//        }
+//
+//    }
+//
+//    fun imgUpload(key : String){
+//        // realtime(boardVO)로 저장되는 값 과 storage에 저장되는 값이 매칭될 수 없음
+//        // 그래서 storage의 이미지 이름을 게시물uid.png 식으로  저장해논다
+//        //  push()가 uid만듬
+//
+//        val storeage = Firebase.storage
+//        val storageRef = storeage.reference
+//        val mountainsRef = storageRef.child("$key.png")
+//
+//        // Get the data from an ImageView as bytes
+//        imgLoad.isDrawingCacheEnabled = true
+//        imgLoad.buildDrawingCache()
+//        val bitmap = (imgLoad.drawable as BitmapDrawable).bitmap
+//        val baos = ByteArrayOutputStream()
+//
+//        // 이미지 압축   png로 못받아옴 가져오는타입 , 압축의 퀄리티 범위
+//        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos)
+//        val data = baos.toByteArray()
+//
+//        // mountainsRef : 이미지를 어디 경로에 설정할것인지
+//        var uploadTask = mountainsRef.putBytes(data)
+//        uploadTask.addOnFailureListener {
+//            // Handle unsuccessful uploads
+//        }.addOnSuccessListener { taskSnapshot ->
+//            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+//            // ...
+//        }
 
 
 
--- app/src/main/java/com/example/dangtime/board/BoardWriteStoryActivity.kt
+++ app/src/main/java/com/example/dangtime/board/BoardWriteStoryActivity.kt
@@ -1,12 +1,49 @@
 package com.example.dangtime.board
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.dangtime.R
+import com.google.android.gms.dynamite.DynamiteModule.VersionPolicy.IVersions
 
 class BoardWriteStoryActivity : AppCompatActivity() {
+    lateinit var imgLoad : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_write_story)
+
+        imgLoad = findViewById(R.id.imgWriteStory)
+        val etContent = findViewById<EditText>(R.id.etWriteStory)
+        val tvTo = findViewById<TextView>(R.id.tvWriteStoryTitle)
+
+        val btnUpload = findViewById<Button>(R.id.btnWriteStoryUpload)
+
+        imgLoad.setOnClickListener{
+
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+
+            launcher.launch(intent)
+        }
+
+    }
+    val launcher =registerForActivityResult(
+        ActivityResultContracts //it에는 result코드와data가 들어있음
+        .StartActivityForResult()){
+
+
+        // result 코드가 OK이라면
+        if (it.resultCode == RESULT_OK){
+            // it이 받아온 data의data에 저장되어있는 이미지 꺼내오기
+            imgLoad.setImageURI(it.data?.data)
+        }
+
+
+
     }
 }
