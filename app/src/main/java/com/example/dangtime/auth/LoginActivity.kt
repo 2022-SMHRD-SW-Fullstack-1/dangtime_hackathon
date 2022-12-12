e405dae9dca7e2260fb98280c50b8e70e178351c
나소연 <ssoy617@gmail.com>
2022-12-12 15:06:49 +0900

    221212

--- app/src/main/java/com/example/dangtime/auth/LoginActivity.kt
+++ app/src/main/java/com/example/dangtime/auth/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.example.dangtime.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import com.example.dangtime.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val imgLoginRegister = findViewById<ImageView>(R.id.imgLoginRegister)
+
+        imgLoginRegister.setOnClickListener {
+            val intent = Intent(this, LocationActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
