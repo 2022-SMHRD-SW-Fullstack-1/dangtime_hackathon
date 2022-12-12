60fe88387cdc5aed4f3abbebd71f9a672ad1efd7
JParkBro <hyeongjoopark0324@gmail.com>
2022-12-12 15:56:27 +0900

    icon , chatlist

--- app/src/main/java/com/example/dangtime/chat/ChatListActivity.kt
+++ app/src/main/java/com/example/dangtime/chat/ChatListActivity.kt
@@ -2,7 +2,10 @@ package com.example.dangtime.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dangtime.R
 
 class ChatListActivity : AppCompatActivity() {
@@ -10,7 +13,34 @@ class ChatListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
 
+        val rvChatList = findViewById<RecyclerView>(R.id.rvChatList)
+        val etChatSearch = findViewById<EditText>(R.id.etChatSearch)
 
-        val tvChat = findViewById<TextView>(R.id.tvChat)
+//        container = rvchatlist
+//        template = chat_list.xml
+//        item
+        val chatList = ArrayList<ChatListVO>()
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요", "12월 11일"))
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요", "1시간전"))
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요", "1시간전"))
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요", "1시간전"))
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요", "1시간전"))
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요", "1시간전"))
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요", "1시간전"))
+        chatList.add(ChatListVO(R.drawable.dogprofile, "우식이", "안녕하세요", "1시간전"))
+
+//        Adapter
+        val adapter = ChatListAdapter(
+            this@ChatListActivity,
+            chatList
+        )
+        rvChatList.adapter = adapter
+        rvChatList.layoutManager = LinearLayoutManager(this@ChatListActivity)
+
+//        Event
+
+        rvChatList
+
+
     }
 }
