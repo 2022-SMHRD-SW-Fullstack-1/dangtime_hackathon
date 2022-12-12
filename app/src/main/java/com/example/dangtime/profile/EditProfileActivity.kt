a2920100a6f1818d1948a586c916f52eed2fe50f
ahaelim <limahae1015@gmail.com>
2022-12-12 16:57:56 +0900

    정보 수정, 탈퇴 페이지

--- app/src/main/java/com/example/dangtime/profile/EditProfileActivity.kt
+++ app/src/main/java/com/example/dangtime/profile/EditProfileActivity.kt
@@ -1,12 +1,23 @@
 package com.example.dangtime.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import com.example.dangtime.R
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        val imgPfEditBack = findViewById<ImageView>(R.id.imgPfEditBack)
+
+        imgPfEditBack.setOnClickListener {
+            val intent = Intent(this@EditProfileActivity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
