715e757cd6bc8816f90f4149784c239b422194a8
Forourhappy <foroh1504@gmail.com>
2022-12-13 12:43:09 +0900

    fragment 연결 완료

--- app/src/main/java/com/example/dangtime/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/dangtime/fragment/home/HomeFragment.kt
@@ -1,35 +1,52 @@
 package com.example.dangtime.fragment.home
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.example.dangtime.R
-import com.example.dangtime.board.BoardChoice
+import com.example.dangtime.post.HomeActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class HomeFragment : Fragment() {
 
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
 
-       val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val fragmentHomeActivity = activity as HomeActivity
+        val bnvHome = view.findViewById<BottomNavigationView>(R.id.bnvHome)
 
 //        val btnWrite = view.findViewById<Button>(R.id.btnWrite)
 
-        val intent = Intent(requireContext(),BoardChoice::class.java)
+//        val intent = Intent(requireContext(), BoardChoice::class.java)
+//
+//        startActivity(intent)
 
-        startActivity(intent)
+//        첫 연결시 fragment 보이게 설정
+        fragmentHomeActivity.changeHomeFragment(1)
 
+        bnvHome.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bnvHomeTab1 -> {
+                    fragmentHomeActivity.changeHomeFragment(1)
+                }
+                R.id.bnvHomeTab2 -> {
+                    fragmentHomeActivity.changeHomeFragment(2)
+                }
+                R.id.bnvHomeTab3 -> {
+                    fragmentHomeActivity.changeHomeFragment(3)
+                }
+            }
+            true
 
+        }
 
 
 
