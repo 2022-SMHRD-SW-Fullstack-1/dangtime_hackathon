2cb8142e7278efdfdc153030243a48980b567ddf
JParkBro <hyeongjoopark0324@gmail.com>
2022-12-14 13:36:11 +0900

    주소검색 kakao API 연결

--- app/src/main/java/com/example/dangtime/auth/SearchLocationActivity.kt
+++ app/src/main/java/com/example/dangtime/auth/SearchLocationActivity.kt
@@ -6,32 +6,52 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.dangtime.R
 
 class SearchLocationActivity : AppCompatActivity() {
+
+    lateinit var etSearchLocation: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_location)
 
-        val etSearchLocation = findViewById<EditText>(R.id.etSearchLocation)
-        val btnSearchSearch = findViewById<Button>(R.id.btnSearchSearch)
+        etSearchLocation = findViewById(R.id.etSearchLocation)
+        val btnSearchNext = findViewById<Button>(R.id.btnSearchNext)
         val imgSearchLocation = findViewById<ImageView>(R.id.imgSearchLocation)
 
+        val imgSearchBack = findViewById<ImageView>(R.id.imgSearchBack)
+        imgSearchBack.setOnClickListener {
+            finish()
+        }
 
-        btnSearchSearch.setOnClickListener {
+        etSearchLocation.isFocusable = false
+        etSearchLocation.setOnClickListener {
+            val intent = Intent(this@SearchLocationActivity, WebSearchActivity::class.java)
+            launcher.launch(intent)
+        }
+
+        imgSearchLocation.setOnClickListener {
+            val intent = Intent(this@SearchLocationActivity, WebSearchActivity::class.java)
+            launcher.launch(intent)
+        }
+
+
+        btnSearchNext.setOnClickListener {
             val intent = Intent(this@SearchLocationActivity, RegisterActivity::class.java)
             intent.putExtra("address", etSearchLocation.text.toString())
             startActivity(intent)
             finish()
-
         }
+    }
 
-        val imgSearchBack = findViewById<ImageView>(R.id.imgSearchBack)
-        imgSearchBack.setOnClickListener {
-//            val intent = Intent(this@SearchLocationActivity, LocationActivity::class.java)
-//            startActivity(intent)
-            finish()
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            if (it.data != null) {
+                val data: String = it.data!!.getStringExtra("data") as String
+                etSearchLocation.setText(data)
+            }
         }
-
     }
 }
