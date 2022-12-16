b928e0e5bd059ab07548d9643b9ccf8e3cf21ac4
ahaelim <limahae1015@gmail.com>
2022-12-16 10:19:39 +0900

    댓글창

--- app/src/main/java/com/example/dangtime/fragment/mypost/MyPostPostAdapter.kt
+++ app/src/main/java/com/example/dangtime/fragment/mypost/MyPostPostAdapter.kt
@@ -101,8 +101,8 @@ class MyPostPostAdapter(
         holder.btnMyPostDel.setOnClickListener {
             FBdatabase.getPostRef().child(postUid[position]).removeValue()
             Toast.makeText(context, "게시물이 삭제되었습니다", Toast.LENGTH_SHORT).show()
-//            val intent = Intent(context, HomeActivity::class.java)
-//            context.startActivity(intent)
+            val intent = Intent(context, HomeActivity::class.java)
+            context.startActivity(intent)
         }
 
     }
